0fd6e87fcbd600c12135b32b0d3ddd61501f23c7
JIgorSilva <jigorsilva@hotmail.com>
2025-01-23 11:09:02 -0300

    feat(testando metodo buscarProdutosId exception)

--- app/src/test/java/com/igor/mercadinho/app/servicesTest/ProdutoServiceTest.java
+++ app/src/test/java/com/igor/mercadinho/app/servicesTest/ProdutoServiceTest.java
@@ -1,24 +1,22 @@
 package com.igor.mercadinho.app.servicesTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-
+import com.igor.mercadinho.app.exception.ProdutoResouceNotFoundException;
+import com.igor.mercadinho.app.model.Produtos;
+import com.igor.mercadinho.app.repository.ProdutoRepository;
+import com.igor.mercadinho.app.services.ProdutosService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import com.igor.mercadinho.app.model.Produtos;
-import com.igor.mercadinho.app.repository.ProdutoRepository;
-import com.igor.mercadinho.app.services.ProdutosService;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class ProdutoServiceTest {
 
@@ -45,7 +43,7 @@ public class ProdutoServiceTest {
         produto.setPreco(new BigDecimal("10.50"));
 
         Produtos produto2 = new Produtos();
-        produto2.setId(null); 
+        produto2.setId(null);
         produto2.setNome("Produto Teste2");
         produto2.setDescricao("Descrição do Produto Teste2");
         produto2.setQuantidade(10);
@@ -81,5 +79,25 @@ public class ProdutoServiceTest {
         assertEquals(2, produtos.size());
         assertEquals("Produto Teste", produtos.get(0).getNome());
         assertEquals("Produto Teste2", produtos.get(1).getNome());
+    }
+
+    @Test
+    void buscarProdutoPorId(){
+        Produtos produtos1 = produtos.get(1);
+        int idProduto = 1;
+        produtos1.setId((long)idProduto);
+        when(produtoRepository.findById(idProduto)).thenReturn(Optional.of(produtos1));
+
+
+        Produtos produtosResponse = produtosService.buscarProdutoPorId(idProduto);
+        RuntimeException produtoNaoExiste = assertThrows(ProdutoResouceNotFoundException.class,()->{
+            produtosService.buscarProdutoPorId(3);
+        });
+
+        assertNotNull(produtosResponse);
+        assertEquals(1,produtosResponse.getId());
+        assertEquals("Produto não encontrado para o ID: "+3,produtoNaoExiste.getMessage());
+        verify(produtoRepository, times(1)).findById(idProduto);
+
     }
 }
