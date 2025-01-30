09f74c207de20aeaeddb783adf4cb61314ce34cc
JIgorSilva <jigorsilva@hotmail.com>
2025-01-30 11:55:45 -0300

    test(teste metodo buscar produto por referencia de nome)

--- app/src/test/java/com/igor/mercadinho/app/servicesTest/ProdutoServiceTest.java
+++ app/src/test/java/com/igor/mercadinho/app/servicesTest/ProdutoServiceTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import com.igor.mercadinho.app.model.Produtos;
@@ -82,4 +83,38 @@ public class ProdutoServiceTest {
         assertEquals("Produto Teste", produtos.get(0).getNome());
         assertEquals("Produto Teste2", produtos.get(1).getNome());
     }
+<<<<<<< Updated upstream
+=======
+
+    @Test
+    void buscarProdutoPorId(){
+        Produtos produtos1 = produtos.get(1);
+        int idProduto = 1;
+        produtos1.setId((long)idProduto);
+        when(produtoRepository.findById(idProduto)).thenReturn(Optional.of(produtos1));
+
+
+        Produtos produtosResponse = produtosService.buscarProdutoPorId(idProduto);
+        RuntimeException produtoNaoExiste = assertThrows(ProdutoResouceNotFoundException.class,()->{
+            produtosService.buscarProdutoPorId(3);
+        });
+
+        assertNotNull(produtosResponse);
+        assertEquals(1,produtosResponse.getId());
+        assertEquals("Produto não encontrado para o ID: "+3,produtoNaoExiste.getMessage());
+        verify(produtoRepository, times(1)).findById(idProduto);
+
+    }
+
+    @Test
+    void buscarProdutoPorString(){
+        String referencia = "Produto Teste";
+        List<Produtos>produtosReferencia= produtos;
+        Mockito.when(produtoRepository.findAll()).thenReturn(produtosReferencia);
+
+        assertNotNull(produtosReferencia,"Lista com produtos");
+        assertEquals(referencia,produtosReferencia.get(0).getNome());
+
+    }
+>>>>>>> Stashed changes
 }
