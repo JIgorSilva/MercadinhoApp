6149f83594c53f58b0c5d2c6ace0e290190e0c57
JIgorSilva <jigorsilva@hotmail.com>
2025-01-30 12:17:22 -0300

    Test(api test produto service test task 18) (#36)

--- app/src/test/java/com/igor/mercadinho/app/servicesTest/ProdutoServiceTest.java
+++ app/src/test/java/com/igor/mercadinho/app/servicesTest/ProdutoServiceTest.java
@@ -1,21 +1,24 @@
 package com.igor.mercadinho.app.servicesTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 
+import com.igor.mercadinho.app.exception.ProdutoResouceNotFoundException;
 import com.igor.mercadinho.app.model.Produtos;
 import com.igor.mercadinho.app.repository.ProdutoRepository;
 import com.igor.mercadinho.app.services.ProdutosService;
@@ -81,5 +84,37 @@ public class ProdutoServiceTest {
         assertEquals(2, produtos.size());
         assertEquals("Produto Teste", produtos.get(0).getNome());
         assertEquals("Produto Teste2", produtos.get(1).getNome());
+    }
+
+    @Test
+    void buscarProdutoPorId(){
+        Produtos produtos1 = produtos.get(1);
+        int idProduto = 1;
+        produtos1.setId((long)idProduto);
+        when(produtoRepository.findById(idProduto)).thenReturn(Optional.of(produtos1));
+
+
+        Produtos produtosResponse = produtosService.buscarProdutoPorId(idProduto);
+        RuntimeException produtoNaoExiste = assertThrows(ProdutoResouceNotFoundException.class,()->{
+            produtosService.buscarProdutoPorId(3);
+        });
+
+        assertNotNull(produtosResponse);
+        assertEquals(1,produtosResponse.getId());
+        assertEquals("Produto não encontrado para o ID: "+3,produtoNaoExiste.getMessage());
+        verify(produtoRepository, times(1)).findById(idProduto);
+
+    }
+
+
+    @Test
+    void buscarProdutoPorString(){
+        String referencia = "Produto Teste";
+        List<Produtos>produtosReferencia= produtos;
+        Mockito.when(produtoRepository.findAll()).thenReturn(produtosReferencia);
+
+        assertNotNull(produtosReferencia,"Lista com produtos");
+        assertEquals(referencia,produtosReferencia.get(0).getNome());
+
     }
 }
