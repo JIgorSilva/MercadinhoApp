b096b91aafe67120d24d525698cde78e5846ead2
JIgorSilva <jigorsilva@hotmail.com>
2025-02-04 10:59:36 -0300

    Test(api services deletar idnome task 20) (#39)
    * feat(criação da develop) (#22)
    
    * fix(Refatorando metodos da Service) (#26)
    
    * fix(Correção configuração de ambiente e dependencias para Teste) (#27)
    
    * feat(Criação dos testes unitarios e de integração com mock da Services) (#28)
    
    * Test(api test produto service test task 18) (#36)
    
    * Deploy(api produção produto deploy fly io task 19) (#38)
    
    * feat(criação da develop)
    
    * Fix(api produtos services task 13) (#23)
    
    * Fix(api configuracao pomxml task 14) (#24)
    
    * Feat(api test produto service test task 15) (#25)
    
    * Api buscar produto por preco (#29)
    
    * feat(criação da develop) (#22)
    
    * Api função buscar por preço
    
    * Delete .vscode/settings.json
    
    * Update ProdutoController.java
    
    ---------
    
    Co-authored-by: JIgorSilva <jigorsilva@hotmail.com>
    
    * Atualização dev (#31)
    
    * d (#32)
    
    * feat(criação da develop) (#22)
    
    * fix(Refatorando metodos da Service) (#26)
    
    * fix(Correção configuração de ambiente e dependencias para Teste) (#27)
    
    * feat(Criação dos testes unitarios e de integração com mock da Services) (#28)
    
    * feat(testando metodo buscarProdutosId exception) (#33)
    
    * deploy(Dockerização e deploy, para ambiente de produção do app)
    
    * Test(api test produto service test task 18) (#35)
    
    * feat(criação da develop) (#22)
    
    * fix(Refatorando metodos da Service) (#26)
    
    * fix(Correção configuração de ambiente e dependencias para Teste) (#27)
    
    * feat(Criação dos testes unitarios e de integração com mock da Services) (#28)
    
    * test(teste metodo buscar produto por referencia de nome)
    
    * deploy(Dockerização e deploy, para ambiente de produção do app)
    
    * New Develop fix (#37)
    
    * fix(Add .env)
    
    * Conflitos fix merge na ProdutosService.java
    
    * deploy(Dockerização e deploy, para ambiente de produção do app) (#34)
    
    * feat(criação da develop) (#22)
    
    * fix(Refatorando metodos da Service) (#26)
    
    * fix(Correção configuração de ambiente e dependencias para Teste) (#27)
    
    * feat(Criação dos testes unitarios e de integração com mock da Services) (#28)
    
    * deploy(Dockerização e deploy, para ambiente de produção do app)
    
    * deploy(Dockerização e deploy, para ambiente de produção do app)
    
    * fix(Add .env)
    
    * Conflitos fix merge na ProdutosService.java
    
    * feat(testando metodo buscarProdutosId exception)
    
    ---------
    
    Co-authored-by: João Vithor <106237379+joaovithorbarros@users.noreply.github.com>
    
    * Fix(api configuracao pomxml task 14) (#24)
    
    * Atualização dev (#31)
    
    * d (#32)
    
    * feat(criação da develop) (#22)
    
    * fix(Refatorando metodos da Service) (#26)
    
    * fix(Correção configuração de ambiente e dependencias para Teste) (#27)
    
    * feat(Criação dos testes unitarios e de integração com mock da Services) (#28)
    
    * feat(testando metodo buscarProdutosId exception) (#33)
    
    * Test(api test produto service test task 18) (#35)
    
    * feat(criação da develop) (#22)
    
    * fix(Refatorando metodos da Service) (#26)
    
    * fix(Correção configuração de ambiente e dependencias para Teste) (#27)
    
    * feat(Criação dos testes unitarios e de integração com mock da Services) (#28)
    
    * test(teste metodo buscar produto por referencia de nome)
    
    * New Develop fix (#37)
    
    * test(teste end point delete. ajuste nas dependencias pom junit, config workspace)
    
    ---------
    
    Co-authored-by: João Vithor <106237379+joaovithorbarros@users.noreply.github.com>

--- app/src/main/java/com/igor/mercadinho/app/services/ProdutosService.java
+++ app/src/main/java/com/igor/mercadinho/app/services/ProdutosService.java
@@ -4,10 +4,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.management.RuntimeErrorException;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.igor.mercadinho.app.exception.ProductWithDifferentIdentifier;
