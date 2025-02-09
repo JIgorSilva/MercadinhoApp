401b3fdca2a83517b5e8b195f30d6a6ed4f763c2
JIgorSilva <jigorsilva@hotmail.com>
2025-02-09 13:22:10 -0300

    fix ajuste bearea token para listar usuarios.

--- app/src/main/java/com/igor/mercadinho/app/controller/UsuarioController.java
+++ app/src/main/java/com/igor/mercadinho/app/controller/UsuarioController.java
@@ -1,12 +1,19 @@
 package com.igor.mercadinho.app.controller;
 
-import com.igor.mercadinho.app.model.Usuario;
-import com.igor.mercadinho.app.services.UsuarioService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.igor.mercadinho.app.model.Usuario;
+import com.igor.mercadinho.app.services.UsuarioService;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
 @RequestMapping("/usuario")
@@ -21,6 +28,7 @@ public class UsuarioController {
     }
 
     @GetMapping("/listar")
+    @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<List<Usuario>> listarTodosUsuarios(){List<Usuario>listaUsuarios = usuarioService.listarUsuarios();
         return  ResponseEntity.ok(listaUsuarios);
     }
