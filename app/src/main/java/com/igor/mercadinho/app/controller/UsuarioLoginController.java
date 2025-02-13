188d5ac91c9793e0bc3e079da74cc58fd7371eb4
JIgorSilva <jigorsilva@hotmail.com>
2025-02-13 02:41:29 -0300

    feat - criação class UsuarioDto para filtro de senha e envio login usuario.

--- app/src/main/java/com/igor/mercadinho/app/controller/UsuarioLoginController.java
+++ app/src/main/java/com/igor/mercadinho/app/controller/UsuarioLoginController.java
@@ -3,6 +3,10 @@ package com.igor.mercadinho.app.controller;
 import com.igor.mercadinho.app.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
@@ -11,6 +15,8 @@ public class UsuarioLoginController {
     private UsuarioService usuarioService;
 
     @PostMapping("/login")
+    public ResponseEntity<String> loginUsuario(@RequestParam String email, @RequestParam String senha){
+        String response =usuarioService.loginUsuario(senha,email);
         return ResponseEntity.ok(response);
     }
 }
