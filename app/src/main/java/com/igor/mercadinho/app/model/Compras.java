e2d0809a0a7fbf8bccfe24194336229c40518704
JIgorSilva <jigorsilva@hotmail.com>
2025-03-05 00:05:54 -0300

    fix - ajustes no imporsts jakarta (#66)

--- app/src/main/java/com/igor/mercadinho/app/model/Compras.java
+++ app/src/main/java/com/igor/mercadinho/app/model/Compras.java
@@ -1,12 +1,24 @@
 package com.igor.mercadinho.app.model;
 
-import com.igor.mercadinho.app.exception.CompraInvalidaItensNullException;
-import com.igor.mercadinho.app.exception.global.CompraInvalidadeNotFoundPrecoUnitario;
-import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.igor.mercadinho.app.exception.CompraInvalidaItensNullException;
+import com.igor.mercadinho.app.exception.global.CompraInvalidadeNotFoundPrecoUnitario;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "compras")
